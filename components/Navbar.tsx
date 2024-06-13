d4dfe928b89d63385f0142b6a52cfe77ea476013
Diego Rodrigo Sanchez Moreno <dsanchez@litebox.ai>
2024-06-13 19:49:19 -0300

    feature: add styles to navbar button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
 // Libs
-import Link from "next/link";
+import Link from "next/link"
+import { Button } from "./"
 
 interface NavItem {
-  title: string;
-  href: string;
+  title: string
+  href: string
 }
 
 const LOGO_TEXT = "DRSM"
@@ -11,34 +12,43 @@ const CTA_LINK_TEXT = "Linkedin"
 const CTA_LINK_URL = "https://www.linkedin.com/in/diego-rodrigo-sanchez-moreno/"
 const NAV_ITEMS: NavItem[] = [
   {
-    title: 'Home',
-    href: '/'
+    title: "Home",
+    href: "/",
   },
   {
-    title: 'About',
-    href: '/about'
+    title: "About",
+    href: "/about",
   },
   {
-    title: 'Experience',
-    href: '/experience'
+    title: "Experience",
+    href: "/experience",
   },
 ]
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 flex items-center justify-between w-full h-16 px-20 bg-black1">
-      {/* LOGO */}
-      <div className="text-4xl text-white font-jacquard">{LOGO_TEXT}</div>
+    <nav className="fixed top-0 left-0 flex items-center justify-center w-full h-16 bg-black1">
+      <div className="flex items-center justify-between w-full px-20 max-w-screen-2xl">
+        {/* LOGO */}
+        <div className="text-4xl text-white font-jacquard">{LOGO_TEXT}</div>
 
-      {/* NAV ITEMS */}
-      <ul className="flex items-center gap-8 text-gray">
-        {NAV_ITEMS.map(navItem => (
-          <li className="hover:text-white hover:cursor-pointer">{navItem.title}</li>
-        ))}
-      </ul>
+        {/* NAV ITEMS */}
+        <ul className="flex items-center gap-8 text-gray">
+          {NAV_ITEMS.map((navItem) => (
+            <li
+              key={navItem.href}
+              className="hover:text-white hover:cursor-pointer"
+            >
+              {navItem.title}
+            </li>
+          ))}
+        </ul>
 
-      {/* CALL TO ACTION */}
-      <Link href={CTA_LINK_URL} className="text-white">{CTA_LINK_TEXT}</Link>
+        {/* CALL TO ACTION */}
+        <Button href={CTA_LINK_URL} target="_blank">
+          {CTA_LINK_TEXT}
+        </Button>
+      </div>
     </nav>
   )
 }
