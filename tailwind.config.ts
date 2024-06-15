a2a4c0af0386166085a092eac77a702f8cc3e548
Diego Rodrigo Sanchez Moreno <dsanchez@litebox.ai>
2024-06-15 14:07:05 -0300

    fix: tailwind stops working randomly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
@@ -37,4 +38,5 @@ const config: Config = {
   },
   plugins: [],
 }
+
 export default config
