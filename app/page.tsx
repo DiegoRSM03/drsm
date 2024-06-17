9eb312d55b6a38d39f72a4ecee56fba326293daa
Diego Rodrigo Sanchez Moreno <dsanchez@litebox.ai>
2024-06-16 22:17:08 -0300

    feature: add next-seo lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 // Sections
 import { About, Footer, LandingHeader, WorkExperience } from "@/sections"
+import { NextSeo } from "next-seo"
+import OpenGraphImage from "@/public/images/original/open-graph-image.png"
 
 export default function Home() {
   return (
     <>
+      <NextSeo
+        title="DRSM - Diego Rodrigo Sanchez Moreno personal website"
+        description="DRSM Software Engineer personal website"
+        openGraph={{
+          title: "DRSM - Personal Website",
+          description: "DRSM Software Engineer personal website",
+          images: [{ url: OpenGraphImage.src }],
+          siteName: "DRSM",
+        }}
+      />
       <LandingHeader />
       <About />
       <WorkExperience />
