c83bc68532c18c8bd15c3e5b0b302dd36be70466
Diego Sanchez <62074686+DiegoRSM03@users.noreply.github.com>
2024-06-17 00:50:56 -0300

    1.1.3 (#25)
    * fix: carousel mobile (#20)
    
    Co-authored-by: Diego Rodrigo Sanchez Moreno <dsanchez@litebox.ai>
    
    * FEATURE: Add next seo lib (#22)
    
    * feature: add next-seo lib
    
    * fix: seo server component
    
    * fix: remove next seo and use native metadata handling
    
    * fix: real mobile styles
    
    ---------
    
    Co-authored-by: Diego Rodrigo Sanchez Moreno <dsanchez@litebox.ai>
    
    * fix: animations last details (#24)
    
    Co-authored-by: Diego Rodrigo Sanchez Moreno <dsanchez@litebox.ai>
    
    ---------
    
    Co-authored-by: Diego Rodrigo Sanchez Moreno <dsanchez@litebox.ai>

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -13,15 +13,15 @@ interface Props {
 export const Slider = ({ images, section }: Props) => {
   return (
     <Swiper
-      autoplay={{ delay: 3500 }}
-      className="w-full h-full"
+      autoplay={{ delay: 3000 }}
+      className="w-full h-full overflow-hidden rounded-md"
       modules={[Autoplay]}
       loop
     >
       {images.map((image, index) => (
         <SwiperSlide
           key={`${section}-${index}-swiper-slider`}
-          className="w-full h-full"
+          className="w-full h-full overflow-hidden rounded-md"
         >
           <Image
             alt={`Image for about ${section} inner section`}
@@ -29,7 +29,7 @@ export const Slider = ({ images, section }: Props) => {
             width={700}
             height={430}
             quality={100}
-            className="h-[220px] max-w-full md:h-[430px] md:w-[700px] md:max-w-[700px] object-cover"
+            className="h-[220px] max-w-full md:h-[430px] md:w-[700px] md:max-w-[700px] object-cover rounded-md overflow-hidden"
           />
         </SwiperSlide>
       ))}
